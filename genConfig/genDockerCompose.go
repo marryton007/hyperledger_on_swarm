72d640daa0fbb05c0168258abca6dab7e93ad676
duwenhui <mr.wenhui@gmail.com>
2017-07-24 11:38:42 +0800

    ca

--- genConfig/genDockerCompose.go
+++ genConfig/genDockerCompose.go
@@ -193,6 +193,7 @@ func GenService(dockerCompose *DockerCompose, domainName string, serviceName str
 			check(err)
 
 		case "ca":
+			
 			serviceHost = "ca" + strconv.Itoa(i)
 			orgId := strconv.Itoa(i + 1)
 			service = &Service{
@@ -209,7 +210,7 @@ func GenService(dockerCompose *DockerCompose, domainName string, serviceName str
       service.Environment[2] = "FABRIC_CA_SERVER_TLS_ENABLED=true"
       service.Environment[3] = "FABRIC_CA_SERVER_TLS_CERTFILE=/etc/hyperledger/fabric-ca-server-config/ca.org" + orgId + "." + domainName + "-cert.pem"
       service.Environment[4] = "FABRIC_CA_SERVER_TLS_KEYFILE=/etc/hyperledger/fabric-ca-server-config/CA" + orgId + "_PRIVATE_KEY"
-			service.Command = "sh -c 'fabric-ca-server start --ca.certfile /etc/hyperledger/fabric-ca-server-config/ca.org" + orgId + "." + domainName + "-cert.pem --ca.keyfile /etc/hyperledger/fabric-ca-server-config/CA" + orgId + "_PRIVATE_KEY -b admin:adminpw -d'"
+			service.Command = "sh -c 'fabric-ca-server start --ca.certfile /etc/hyperledger/fabric-ca-server-config/ca.org" + orgId + "." + domainName + "-cert.pem --ca.keyfile /etc/hyperledger/fabric-ca-server-config/CA" + orgId + "_PRIVATE_KEY -b admin:adminpw -d --db.type mysql --db.tls.certfiles ApsaraDB-CA-Chain.pem  --db.datasource blockchain:YW346abB2017_cQaz@tcp(rm-2ze81ji2iw67g5j74.mysql.rds.aliyuncs.com:3306)/fabric_ca_one?parseTime=true&tls=custom'"
 			service.Volumes = make([]string, 1)
 			service.Volumes[0] = "./crypto-config/peerOrganizations/org" + orgId + "." + domainName + "/ca/:/etc/hyperledger/fabric-ca-server-config"
 			//service.Volumes[1] = "./fabric-ca-server/:/etc/hyperledger/fabric-ca-server"
